807c78fd1c387456d3b79ca9d9543e8702143cde
filipmlynarski <fmynarski@gmail.com>
2018-12-04 15:49:23 +0100

    regex is cool

--- day_03/day_3_part_1.py
+++ day_03/day_3_part_1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import re
 
 file = open('puzzle', 'r')
 puzzle = file.read().splitlines()
@@ -7,20 +8,7 @@ file.close()
 suit = np.zeros((1000, 1000))
 
 for square in puzzle:
-	square = square.split()[-2:]
-	x, y = [int(i) for i in square[0][:-1].split(',')]
-	width, height = [int(i) for i in square[1].split('x')]
-	
+	ID, x, y, width, height = map(int, re.findall(r'-?\d+', square))
 	suit[y: y + height, x: x + width] += 1
-
-for square in puzzle:
-	claim_id = square.split()[0][1:]
-	square = square.split()[-2:]
-	x, y = [int(i) for i in square[0][:-1].split(',')]
-	width, height = [int(i) for i in square[1].split('x')]
-	
-	if np.all(suit[y: y + height, x: x + width] == 1):
-		print(claim_id)
-		break
 
 print(np.count_nonzero(suit>1))
