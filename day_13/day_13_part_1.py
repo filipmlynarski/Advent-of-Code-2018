8b45613e22dab2b3acc082f29294477108fd49f8
filipmlynarski <fmynarski@gmail.com>
2018-12-13 06:57:01 +0100

    day 13 solutions

---
+++ day_13/day_13_part_1.py
@@ -0,0 +1,76 @@
+puzzle = open('puzzle', 'r').read().splitlines()
+
+def clear(inp):
+	ret = []
+	players = []
+	for y in range(len(inp)):
+		ret.append([])
+		for x in range(len(inp[y])):
+			if inp[y][x] in ['>', '<']:
+				players.append([[y, x], inp[y][x], 0])
+				ret[-1].append('-')
+			elif inp[y][x] in ['^', 'v']:
+				players.append([[y, x], inp[y][x], 0])
+				ret[-1].append('|')
+			else:
+				ret[-1].append(inp[y][x])
+	return ret, players
+
+def print_track():
+	for y in range(len(track)):
+		for x in range(len(track[y])):
+			player = [i for i in players if [y, x] == i[0]]
+			if len(player):
+				print(player[0][1], end='')
+			else:
+				print(track[y][x], end='')
+		print()
+
+def move(player):
+	if player[1] == '>':
+		player[0][1] += 1
+	elif player[1] == '<':
+		player[0][1] -= 1
+	elif player[1] == '^':
+		player[0][0] -= 1
+	elif player[1] == 'v':
+		player[0][0] += 1
+	return player
+
+turns = [{'>': '^', '^': '<', '<': 'v', 'v': '>'},
+		 {'>': '>', 'v': 'v', '<': '<', '^': '^'},
+		 {'>': 'v', 'v': '<', '<': '^', '^': '>'}]
+
+track, players = clear(puzzle)
+
+while True:
+	for player in sorted(players, key=lambda x: x[0]):
+		if track[player[0][0]][player[0][1]] == '\\':
+			if player[1] == '>':
+				player[1] = 'v'
+			elif player[1] == '^':
+				player[1] = '<'
+			elif player[1] == '<':
+				player[1] = '^'
+			elif player[1] == 'v':
+				player[1] = '>'
+
+		elif track[player[0][0]][player[0][1]] == '/':
+			if player[1] == '>':
+				player[1] = '^'
+			elif player[1] == '^':
+				player[1] = '>'
+			elif player[1] == '<':
+				player[1] = 'v'
+			elif player[1] == 'v':
+				player[1] = '<'
+
+		elif track[player[0][0]][player[0][1]] == '+':
+			player[1] = turns[player[2]][player[1]]
+			player[2] = (player[2] + 1) % len(turns)
+
+		player = move(player)
+		
+		if [i[0] for i in players].count(player[0]) > 1:
+			print(','.join(map(str, player[0][::-1])))
+			exit()
